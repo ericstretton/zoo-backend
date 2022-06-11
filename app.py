fda24c5122833e6f5ed4034ebe01cad609fa431f
ericstretton <eric.stretton@gmail.com>
2022-06-11 10:37:08 -0600

    Implemented endpoint stubs

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-from db_helpers import *
-from flask import Flask, Response, jsonify, request 
-from flask_cors import CORS
-
-
-
+from cProfile import run
+from helpers.db_helpers import run_query
+from flask import Flask, jsonify, request 
 
 app = Flask(__name__)
 animals = []
 
 @app.get('/api/animals')
 def animals_get():
-    args = request.args
-    animal_id = args.get('animalName')
-    if animal_id == None:
-        return jsonify(animals), 200
-    else:
-        return jsonify(animals[animal_id]), 200
+    # TODO: db select
+    animal_list = run_query()
+    return jsonify(animal_list), 200
+    
     
 
 @app.post('/api/animals')
 def animals_post():
-    animal_data = request.json
-    print(animal_data)
-    if not animal_data.get('animalName'):
-        return jsonify("Missing required field: Animal Name"), 422
-    if not animal_data.get('imageURL'):
+    data = request.json
+    animal_name = data.get('animalName')
+    image_url = data.get('imageURL')
+    if not animal_name:
+        return jsonify("Missing required argument: animalName"), 422
+    if not image_url:
         return jsonify("Missing required field: imageURL"), 422
-    run_query("INSERT INTO user (username, age) VALUES(?,?)", 
-        [animal_data.get('animalName'), animal_data.get('animalURL')])
+    # TODO: Error checking the actual values for the arguments
+    
+    # TODO: DB write
+    
+    # run_query("INSERT INTO animal (animalName, imageURL) VALUES(?,?)", 
+    #     [data.get('animalName'), data.get('animalURL')])
 
